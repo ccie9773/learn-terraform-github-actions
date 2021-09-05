28507ae0b530a97c7f2a621d4e6e29a22a5f5680
Jack Yu <jackyu@outlook.com>
2021-09-05 21:53:58 +1000

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -2,17 +2,17 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.26.0"
+      version = ">=3.26.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.0.1"
+      version = ">=3.0.1"
     }
   }
   required_version = ">= 0.14"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "cpoc"
 
     workspaces {
       name = "gh-actions-demo"
@@ -22,7 +22,7 @@ terraform {
 
 
 provider "aws" {
-  region = "us-west-2"
+  region = "ap-southesst-2"
 }
 
 
@@ -30,7 +30,7 @@ provider "aws" {
 resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
+  ami                    = "ami-0567f647e75c7bc05 "
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
